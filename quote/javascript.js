bf0598c44608dd274c7e15e1eea37daa35901841
Alyson Stiffel <alysonstiffel@gmail.com>
2017-09-29 12:45:05 -0400

    added quotes

---
+++ quote/javascript.js
@@ -0,0 +1,19 @@
+var quotes = [
+  'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. -Helen Keller'
+  'Keep love in your heart. A life without it is like a sunless garden when the flowers are dead. -Oscar Wilde'
+  'It is during our darkest moments that we must focus to see the light. -Aristotle'
+  'Try to be a rainbow in someone\'s cloud. -Maya Angelou'
+  'Find a place inside where there\'s joy, and the joy will burn out the pain. -Joseph Campbell'
+  'Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment. -Buddha'
+  'The only thing necessary for the triumph of evil is for good men to do nothing. -Edmund Burke'
+  'One of the most beautiful qualities of true friendship is to understand and to be understood. -Lucius Annaeus Seneca'
+  'Love is composed of a single soul inhabiting two bodies. -Aristotle'
+  'Do not go where the path may lead, go instead where there is no path and leave a trail. -Ralph Waldo Emerson'
+  'In three words I can sum up everything I\'ve learned about life: it goes on. -Robert Frost'
+  'Change your thoughts and you change your world. -Norman Vincent Peale'
+  'Education is the most powerful weapon which you can use to change the world. -Nelson Mandela'
+  'The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails. -William Arthur Ward'
+  'Happiness resides not in possessions, and not in gold, happiness dwells in the soul. -Democritus'
+  'Life\'s most persistent and urgent question is, \'What are you doing for others?\' -Martin Luther King, Jr.'
+]
+
