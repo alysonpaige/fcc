04590d7be0a6bcb2a2d6e006f4488fe1ad1e69d7
Alyson Stiffel <alysonstiffel@gmail.com>
2017-11-08 10:32:00 -0500

    api js

--- weather/javascript.js
+++ weather/javascript.js
@@ -1,1 +1,10 @@
 var api = "https://fcc-weather-api.glitch.me/api/current?";
+var lat, lon;
+var tempUnit = 'C';
+var currentTempInCelsius;
+
+$( document ).ready(function(){
+  if (navigator.geolocation) {
+
+  }
+});
